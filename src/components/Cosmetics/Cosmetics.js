3b5b94da05880fea2411cbebe9ab28750991a37c
sohanur28 <gsrahman28@gmail.com>
2023-03-31 14:56:07 +0600

    import-export

---
+++ src/components/Cosmetics/Cosmetics.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { add } from '../../utilities/calculate';
+// import add from '../../utilities/calculate';
+
+const Cosmetics = () => {
+
+    const first = 55;
+    const second = 66;
+    const total = add(first, second);
+
+    return (
+        <div>
+            <h1>Welcome to my cosmetics store</h1>
+            <p>total: {total}</p>
+        </div>
+    );
+};
+
+export default Cosmetics;
