054afe0b768531e60eb0c813d231061edabadd93
sohanur28 <gsrahman28@gmail.com>
2023-03-31 15:16:39 +0600

    dataGenerate1

--- src/components/Cosmetics/Cosmetics.js
+++ src/components/Cosmetics/Cosmetics.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import { add } from '../../utilities/calculate';
-// import add from '../../utilities/calculate';
+import Cosmetic from '../Cosmetic/Cosmetic';
 
 const Cosmetics = () => {
-
-    const first = 55;
-    const second = 66;
-    const total = add(first, second);
+    const cosmetics = [
+        {id: 1,  name: 'Alta', price: 100},
+        {id: 2,  name: 'Palis', price: 200},
+        {id: 3,  name: 'malis', price: 300},
+        {id: 4,  name: 'balis', price: 400},
+        {id: 5,  name: 'nalis', price: 500},
+    ]
 
     return (
         <div>
             <h1>Welcome to my cosmetics store</h1>
-            <p>total: {total}</p>
+            {
+                cosmetics.map(cosmetic => <Cosmetic 
+                    key={cosmetic.id}
+                    cosmetic={cosmetic}
+                    ></Cosmetic>)
+            }
         </div>
     );
 };
